822222b0c3a352226cc907cf938cca8c24d59f55
Cam McLeod <cammcleod3@gmail.com>
2019-06-13 16:13:17 -0700

    Initial Commit

---
+++ FlickrTags/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrTags
+//
+//  Created by Cameron Mcleod on 2019-06-13.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
